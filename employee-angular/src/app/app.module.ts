342a8abe47f521ccd711a5639a440cd265086898
Jeffe Otto <jeffe.otto@gmail.com>
2020-10-29 17:57:05 +0100

    Final
    Final

--- employee-angular/src/app/app.module.ts
+++ employee-angular/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    DashboardComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- employee-angular/src/app/employees/employees.component.ts
+++ employee-angular/src/app/employees/employees.component.ts
@@ -1,17 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import {Employee} from '../employee';
-import {EMPLOYEES} from '../employee-fakedata';
+import { Location } from '@angular/common';
+import{EmployeeService} from '../employee.service';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
+
+
 export class EmployeesComponent implements OnInit {
- employees = EMPLOYEES;
-  constructor() { }
+  employee: Employee;
+ employees: Employee[];
+  constructor(private employeeService: EmployeeService,
+    private location: Location) { }
 
   ngOnInit(): void {
+    this.getEmployees();
   }
 
+  add(name: string,secondName:string,position:string,salary:number): void {
+   
+    if (!name) { return; }
+    this.employeeService.addEmployee({ name,secondName,position,salary } as Employee)
+      .subscribe(employee => {
+        this.employees.push(employee);
+      });
+  }
+
+   getEmployees(): void{
+     this.employeeService.getEmployees()
+     .subscribe(employees => this.employees = employees);
+   }
+
+   delete(employee: Employee): void {
+    this.employees = this.employees.filter(h => h !== employee);
+    this.employeeService.deleteEmployee(employee).subscribe();
+  }
+
+  save(): void {
+    this.employeeService.updateEmployee(this.employee)
+     
+  }
 }
